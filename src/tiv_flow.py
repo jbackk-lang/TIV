63cb17e2ab7dd46083064e6bd8c9e963f99dbfb5
Jacek Kielich <jbackk@gmail.com>
2026-09-05 01:30:15 +0200

    Delete src directory

--- src/tiv_flow.py
+++
@@ -1,6 +0,0 @@
-def flow_update(tiv_value: float, inflow: float, outflow: float, source: float) -> float:
-    """
-    Prosty krok przepływu TIV w węźle:
-    dTIV/dt = inflow - outflow + source
-    """
-    return tiv_value + inflow - outflow + source
